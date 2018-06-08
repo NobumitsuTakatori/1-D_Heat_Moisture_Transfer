50cc13d8ce09b04ec41fabed4c6aef9d00e6b5d6
NOBUMITSU-PC\nobumitsu <takatori.nobumitsu.62a@st.kyoto-u.ac.jp>
2018-06-08 17:54:15 +0900

    作業途中ですが、物性値を選択するクラスと、選択した物性値を流量計算用に変換するクラスをまとめたものとしてPropertyConversionというファイルを作成しました。 それに伴い物性値のファイルを一部加筆修正しました。 物性値のファイルがどのような形式で書かれているかわからない状況で、物性値を取り出し、流量計算用に物性値を変換するという工程に少し悩んでおります。

--- vapour_pressure.py
+++ vapour_pressure.py
@@ -2,29 +2,25 @@
 import numpy as np
 #import matplotlib.pyplot as plt
 
-def Pv_cal(Tem, Miu):
+def Pv(Tem, Miu):
     Rv = 8316.96/18.016
     RH = np.exp(Miu/Rv/Tem)
-    Pvs= Pvs_cal(Tem)
-    Pv = RH *Pvs    
+    Pv = RH *Pvs(Tem)    
     return Pv
     
-def Pvs_cal(Tem):
+def Pvs(Tem):
     EW =-5800.22060/Tem + 1.3914993 -4.8640239E-2*Tem + 4.1764768E-5*(Tem**2.0)\
     -1.4452093E-8*(Tem**3.0)+6.5459673*np.log(Tem)
-    Pvs = np.exp(EW)
-    return Pvs 
+    return np.exp(EW) 
 
-def DPvs_cal(Tem):
-    Pvs = Pvs_cal(Tem)    
+def DPvs(Tem):
     DP=10.795740*273.160/Tem/Tem-5.0280/Tem/np.log(10.0)\
     +(1.50475E-4)*8.2969/273.16*np.log(10.0)\
     *(10.0**(-8.29690*(Tem/273.160 -1.0)))\
     +(0.42873E-3)*4.769550*273.160/Tem/Tem*np.log(10.0)\
     *(10.0**(4.769550*(1.0 -273.160/Tem)))
-    DPvs = Pvs*DP*np.log(10.0)
+    DPvs = Pvs(Tem) *DP*np.log(10.0)
     return DPvs
-
 
 ###     計算・グラフの描画      ###
 #plt.xscale("log")
